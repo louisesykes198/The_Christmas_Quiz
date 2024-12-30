a8c41eda35e05232176b42dbab3a4c92f296c8a7
Louise Sykes <louisesykes198@yahoo.com>
2024-12-30 15:11:53 +0000

    Add function to show question in javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,71 +2,72 @@
 const questions = [
     {
         question: 'What did the other reindeer not let Rudolph do because of his shiny red nose?',
-        possibleAnswers: ["Join in any reindeer games", "Play football", "Fly the sleigh", "Eat mince pies"],
-        correctAnswer: 'Join in any reindeer games',
+        answers: ["Join in any reindeer games", "Play football", "Fly the sleigh", "Eat mince pies"],
+        correctAnswer: '0',
     },
     {
         question: 'How many ghosts show up in A Christmas Carol?',
-        possibleAnswers: ["Three", "Two", "Four", "Five"],
-        correctAnswer: ' Four',
+        answers: ["Three", "Two", "Four", "Five"],
+        correctAnswer: ' 2',
     },
     {
         question: 'What do people traditionally put on top of a Christmas tree?',
-        possibleAnswers: ["A star", "An angel", "Tinsel", "An elf"],
-        correctAnswer: 'An angel',
+        answers: ["A star", "An angel", "Tinsel", "An elf"],
+        correctAnswer: '1',
     },
     {
         question: 'In Home Alone where are the McCallisters going on vacation when they leave Kevin behind?',
-        possibleAnswers: ["Paris", "India", "Lapland", "New York"],
-        correctAnswer: 'Paris',
+        answers: ["Paris", "India", "Lapland", "New York"],
+        correctAnswer: '0',
     },
     {
         question: 'What words follow "Silent Night" in the song?',
-        possibleAnswers: ["Christmas Night", "Thursday Night", "Holy Night", "Quite Night"],
-        correctAnswer: 'Holy night',
+        answers: ["Christmas Night", "Thursday Night", "Holy Night", "Quite Night"],
+        correctAnswer: '2',
     },
     {
         question: 'Which Christmas song contains the lyric "Everyone dancing merrily in the new old-fashioned way?"',
-        possibleAnswers: ["Rocking Around The Christmas Tree", "White Christmas", "Do They Know It's Christmas", "Jingle Bell Rock"],
-        correctAnswer: 'Rocking Around The Christmas Tree',
+        answers: ["Rocking Around The Christmas Tree", "White Christmas", "Do They Know It's Christmas", "Jingle Bell Rock"],
+        correctAnswer: '0',
     },
     {
         question: 'What are you supposed to do when you find yourself under the mistletoe?',
-        possibleAnswers: ["Dance", "Kiss", "Shake Hands", "Drink A shot"],
-        correctAnswer: 'Kiss',
+        answers: ["Dance", "Kiss", "Shake Hands", "Drink A shot"],
+        correctAnswer: '1',
     },
     {
         question: 'Which one of Santas reindeer has the same name as another holiday mascot?',
-        possibleAnswers: ["Prancer", "Dasher", "Cupid", "Blitzen"],
-        correctAnswer: 'Cupid',
+        answers: ["Prancer", "Dasher", "Cupid", "Blitzen"],
+        correctAnswer: '2',
     },
     {
         question: 'Whats the name of the main villain in The Nightmare Before Christmas?',
-        possibleAnswers: ["Sally", "Dr. Finkelstein", "Oogie Boogie", "Mayor"],
-        correctAnswer: 'Oogie Boogie',
+        answers: ["Sally", "Dr. Finkelstein", "Oogie Boogie", "Mayor"],
+        correctAnswer: '2',
     },
     {
         question: 'According to the song, what did my true love give to me on the eighth day of Christmas?',
-        possibleAnswers: ["Partridge in a pear tree", "Swans a swimming", "Ladies Dancing", "maids a-milking"],
-        correctAnswer: 'maids a-milking',
+        answers: ["Partridge in a pear tree", "Swans a swimming", "Ladies Dancing", "maids a-milking"],
+        correctAnswer: '3',
     }
 ];
 
 // variables for functions
 const questionElement = document.getElementById("question");
-const possibleAnswers = document.getElementById("possibleAnswers");
+const answers = document.getElementById("possibleAnswers");
 const correctAnswer = document.getElementById("correctAnswer");
-let latestQuestion = 0;
+let currentQuestion = 0;
 let score = 0;
+let time = 0;
 // function to start quiz
 function startQuiz(){
-    let latestQuestion = 0;
+    let currentQuestion = 0;
     let score = 0;
-    showQuestion();
-}
+    let time = 0;
+  }
 // function to show questions
-function showQuestion(){
-    latestQuestion = questions[latestQuestion];
-    let question =latestQuestion + 1;
-    questionElement.innerHTML = question + ". " + latestQuestion.question;
-}
+const questionEl = document.getElementById("questions");
+const answerButtons = document.querySelectorAll(".answer-btn");
+let scoreEl = document.getElementById("score");
+let timeEl = document.getElementById("time");
+startQuiz()
